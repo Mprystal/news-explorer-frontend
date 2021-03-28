3737ec5591780f14df4234c5211fead91cbf5ace
Mprystal <mjprystal@gmail.com>
2021-03-28 14:15:23 -0400

    stretch image to cover vw, fix header

--- news-explorer/src/components/Footer/Footer.js
+++ news-explorer/src/components/Footer/Footer.js
@@ -9,11 +9,11 @@ function Footer() {
         <footer className='footer'>
             <p className="footer__paragraph">&copy; 2020 Supersite, Powered by News API</p>
          <div className='footer__container'>
-             <div className='footer__link-container' >
+             <div>
                 <Link className='footer__link' to='/'>Home</Link>
                 <a className='footer__link' href='https://practicum.yandex.com/web/' >Practicum by Yandex</a>
              </div>
-             <div className='footer__social-container'>
+             <div>
                 <a className='footer__link-social' href="https://github.com/Mprystal"><img src={git} alt='github account' /></a>
                 <a className='footer__link-social' href="https://www.facebook.com/"><img src={facebook} alt='facebook account' /></a>
              </div>
